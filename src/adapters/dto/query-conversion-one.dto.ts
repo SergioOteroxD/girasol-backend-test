61d5a4e34291adf651c272e07979d314bf0f77a1
Sergio Otero <sergiootero536@gmail.com>
2025-02-26 23:34:28 -0500

    feature(dto): enhance DTOs with Swagger API properties for better documentation

--- src/adapters/dto/query-conversion-one.dto.ts
+++ src/adapters/dto/query-conversion-one.dto.ts
@@ -1,14 +1,18 @@
 import { IsEnum, IsNotEmpty, IsNumberString } from 'class-validator';
 import { Ecurrency } from '../../commons/enum/currency.enum';
+import { ApiProperty } from '@nestjs/swagger';
 export class QueryConversionOneDto {
+  @ApiProperty({})
   @IsNotEmpty({ message: 'El parámetro "from" es obligatorio' })
   @IsEnum(Ecurrency, { message: 'Debe coincidir con Ecurrency' })
   from: Ecurrency;
 
+  @ApiProperty({})
   @IsNotEmpty({ message: 'El parámetro "to" es obligatorio' })
   @IsEnum(Ecurrency, { message: 'Debe coincidir con Ecurrency' })
   to: Ecurrency;
 
+  @ApiProperty({})
   @IsNumberString({}, { message: 'El parámetro "amount" debe ser un número' })
   @IsNotEmpty({ message: 'El parámetro "from" es obligatorio' })
   amount: number;
